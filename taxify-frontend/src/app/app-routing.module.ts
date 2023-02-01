1dd67b8edc8f17c22c3bd9cc9242e3ceec059dd5
Uros <prijovic.uros13@gmail.com>
2023-02-01 23:28:15 +0100

    bug-fix for route redirection

--- taxify-frontend/src/app/app-routing.module.ts
+++ taxify-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/users/all', pathMatch: 'full' },
-];
+const routes: Routes = [{ path: '', redirectTo: '/maps', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
